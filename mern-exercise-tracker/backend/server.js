ea81a9c412ec413a9cca9b1b06b78cd5811a93f8
Sean Garnett <sean.garnett@protonmail.com>
2021-04-14 17:07:20 -0400

    established Server MangoDB connection

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require ('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
 
@@ -9,6 +9,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true});
+client.connect(err => {
+    console.log("MongoDB database connection established successfully");
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
